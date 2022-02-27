11f5b2864068625d7f88d751d7adf4c08e397ebb
ErmekZhumagulov <erm.zhumagulov@gmail.com>
2022-02-28 03:10:09 +0600

    adding test button below

---
+++ js/buttons.js
@@ -0,0 +1,28 @@
+// wc
+const ButtonClickWc = document.getElementById('button-wc');
+const ButtonClickAllWc = document.querySelectorAll('.path-wc');
+const ButtonClickAllWcText = document.querySelectorAll('.text-wc');
+ButtonClickAllWc.forEach(elemWc => {
+    function funcButtonClickWc(event) {
+        if (event.target.closest('#button-wc')) {
+            ButtonClickWc.classList.add('сlickToChangeButtonColor');
+        }
+        if (!event.target.closest('#button-wc')) {
+            ButtonClickWc.classList.remove('сlickToChangeButtonColor');
+        }
+        if (event.target.closest('#button-wc')) {
+            elemWc.classList.add('сlickToChangeColor');
+        }
+        ButtonClickWc.removeEventListener("click", funcButtonClickWc);
+    }
+    document.addEventListener("click", funcButtonClickWc);
+});
+ButtonClickAllWcText.forEach(elemWcText => {
+    function funcButtonClickWcText(event) {
+        if (event.target.closest('#button-wc')) {
+            elemWcText.classList.add('сlickToChangeTextColor');
+        }
+        ButtonClickWcText.removeEventListener("click", funcButtonClickWcText);
+    }
+    document.addEventListener("click", funcButtonClickWcText);
+});
